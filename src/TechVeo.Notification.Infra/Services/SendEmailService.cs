28326bc0434f549366c0a8007e34ac735a95eb05
ldssborges <ldssborges912@gmail.com>
2026-02-02 22:51:03 -0300

    feat: adjust packages and inject dependencies

--- src/TechVeo.Notification.Infra/Services/SendEmailService.cs
+++ src/TechVeo.Notification.Infra/Services/SendEmailService.cs
@@ -3,46 +3,51 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using TechVeo.Notification.Application.Services;
+using TechVeo.Shared.Application.Aws;
 
 namespace TechVeo.Notification.Infra.Services;
 
 public class SendEmailService : ISendEmailService
 {
     private readonly ILogger<SendEmailService> _logger;
+    private readonly AmazonSimpleEmailServiceClient _client;
+    private readonly string _fromAddress;
 
-    public SendEmailService(ILogger<SendEmailService> logger)
+    public SendEmailService(ILogger<SendEmailService> logger, IOptions<AwsOptions> awsOptions,IConfiguration configuration)
     {
         _logger = logger;
+
+        _fromAddress = configuration["Email:FromAddress"]!;
+
+        _client = new AmazonSimpleEmailServiceClient(awsOptions.Value.AccessKey, awsOptions.Value.SecretKey,
+                  RegionEndpoint.GetBySystemName(awsOptions.Value.Region));
     }
 
     public async Task SendAsync(string toAddress, string subject, string bodyHtml)
     {
-        var fromAddress = "seu-email@verificado.com";
-
-        using (var client = new AmazonSimpleEmailServiceClient("SEU_ACCESS_KEY", "SEU_SECRET_KEY", RegionEndpoint.USEast1))
+        var sendRequest = new SendEmailRequest
         {
-            var sendRequest = new SendEmailRequest
+            Source = _fromAddress,
+            Destination = new Destination { ToAddresses = new() { toAddress } },
+            Message = new Message
             {
-                Source = fromAddress,
-                Destination = new Destination { ToAddresses = new() { toAddress } },
-                Message = new Message
-                {
-                    Subject = new Content(subject),
-                    Body = new Body { Html = new Content(bodyHtml) }
-                }
-            };
+                Subject = new Content(subject),
+                Body = new Body { Html = new Content(bodyHtml) }
+            }
+        };
 
-            try
-            {
-                var response = await client.SendEmailAsync(sendRequest);
-                Console.WriteLine("Email enviado com sucesso! ID: " + response.MessageId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Erro ao enviar: " + ex.Message);
-            }
+        try
+        {
+            var response = await _client.SendEmailAsync(sendRequest);
+            Console.WriteLine("Email enviado com sucesso! ID: " + response.MessageId);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao enviar: " + ex.Message);
         }
     }
 }
